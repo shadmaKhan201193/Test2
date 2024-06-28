1714171fc6bf92b6177b61a814bbde590bdef3d4
shadmaKhan201193 <73832663+shadmaKhan201193@users.noreply.github.com>
2024-06-28 11:53:05 +0530

    Add files via upload

---
+++ spring-drools-master/spring-drools-master/src/main/java/com/javatechie/spring/drools/api/MegaOfferController.java
@@ -0,0 +1,63 @@
+package com.javatechie.spring.drools.api;
+
+import java.util.List;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.jbpm.test.JBPMHelper;
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.manager.RuntimeEngine;
+import org.kie.api.runtime.manager.RuntimeEnvironmentBuilder;
+import org.kie.api.runtime.manager.RuntimeManager;
+import org.kie.api.runtime.manager.RuntimeManagerFactory;
+import org.kie.api.task.TaskService;
+import org.kie.api.task.model.TaskSummary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MegaOfferController {
+	@Autowired
+	private RuntimeManager createRuntimeManager;
+
+
+	@GetMapping("/order")
+	public String orderNow() {
+	
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks.getKieClasspathContainer();
+		KieBase kbase = kContainer.getKieBase("kbase");
+
+		RuntimeManager manager = createRuntimeManager(kbase);
+		RuntimeEngine engine = manager.getRuntimeEngine(null);
+		KieSession ksession = engine.getKieSession();
+		TaskService taskService = engine.getTaskService();
+
+		ksession.startProcess("com.sample.bpmn.hello");
+
+		// let john execute Task 1
+		List<TaskSummary> list = taskService.getTasksAssignedAsPotentialOwner("john", "en-UK");
+		
+		
+		
+		return "ss";
+	}
+
+
+	public  RuntimeManager createRuntimeManager(KieBase kbase) {
+		JBPMHelper.startH2Server();
+		JBPMHelper.setupDataSource();
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("org.jbpm.persistence.jpa");
+		RuntimeEnvironmentBuilder builder = RuntimeEnvironmentBuilder.Factory.get()
+			.newDefaultBuilder().entityManagerFactory(emf)
+			.knowledgeBase(kbase);
+		return RuntimeManagerFactory.Factory.get()
+			.newSingletonRuntimeManager(builder.get(), "com.sample:example:1.0");
+	}
+
+}
---
+++ spring-drools-master/spring-drools-master/src/main/java/com/javatechie/spring/drools/api/SpringDroolsApplication.java
@@ -0,0 +1,17 @@
+package com.javatechie.spring.drools.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class SpringDroolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDroolsApplication.class, args);
+	}
+
+	
+
+}
